3819ebd5f55dd63460ac76ffa361ce6aabf220ef
Lorenzo Ortiz <lorenzo.ortiz@gmail.com>
2026-04-02 09:17:35 +0200

    Add dynamic monitor detection and keyboard shortcuts in menu

--- MonitorToggleTray/Form1.cs
+++ MonitorToggleTray/Form1.cs
@@ -9,12 +9,10 @@ public partial class Form1 : Form
 {
     private NotifyIcon notifyIcon;
     private ContextMenuStrip contextMenu;
-    private bool isMonitor1On = false;
-    private bool isMonitor2On = false;
+    private bool[] monitorStates = new bool[4]; // Support up to 4 monitors
+    private int monitorCount = 0;
 
     private const int WM_HOTKEY = 0x0312;
-    private const int HOTKEY_ID1 = 1;
-    private const int HOTKEY_ID2 = 2;
     private const uint MOD_CONTROL = 0x0002;
     private const uint MOD_ALT = 0x0001;
 
@@ -27,24 +25,60 @@ public partial class Form1 : Form
     public Form1()
     {
         InitializeComponent();
+        DetectMonitors();
         SetupTrayIcon();
-        RegisterHotKey(this.Handle, HOTKEY_ID1, MOD_CONTROL | MOD_ALT, (uint)Keys.F1);
-        RegisterHotKey(this.Handle, HOTKEY_ID2, MOD_CONTROL | MOD_ALT, (uint)Keys.F2);
+        RegisterHotKeys();
+    }
+
+    private void DetectMonitors()
+    {
+        // Detect connected monitors (using Screen.AllScreens as reference)
+        monitorCount = Math.Min(Screen.AllScreens.Length, 4);
+        if (monitorCount == 0) monitorCount = 1; // At least 1 monitor assumed
+    }
+
+    private void RegisterHotKeys()
+    {
+        Keys[] fKeys = { Keys.F1, Keys.F2, Keys.F3, Keys.F4 };
+        for (int i = 0; i < monitorCount; i++)
+        {
+            RegisterHotKey(this.Handle, i + 1, MOD_CONTROL | MOD_ALT, (uint)fKeys[i]);
+        }
     }
 
     private void SetupTrayIcon()
     {
         notifyIcon = new NotifyIcon();
-        notifyIcon.Icon = SystemIcons.Information; // Icono tipo información, puedes cambiar a uno personalizado
+        notifyIcon.Icon = SystemIcons.Information;
         notifyIcon.Text = "Monitor Toggle";
+        
         contextMenu = new ContextMenuStrip();
-        contextMenu.Items.Add("Toggle Monitor 1", null, (s, e) => Toggle1());
-        contextMenu.Items.Add("Toggle Monitor 2", null, (s, e) => Toggle2());
-        contextMenu.Items.Add("Turn On Monitor 1", null, (s, e) => TurnOn1());
-        contextMenu.Items.Add("Turn Off Monitor 1", null, (s, e) => TurnOff1());
-        contextMenu.Items.Add("Turn On Monitor 2", null, (s, e) => TurnOn2());
-        contextMenu.Items.Add("Turn Off Monitor 2", null, (s, e) => TurnOff2());
+
+        // Add dynamic menu items for each detected monitor
+        for (int i = 1; i <= monitorCount; i++)
+        {
+            Keys[] fKeys = { Keys.F1, Keys.F2, Keys.F3, Keys.F4 };
+            string shortcut = $"Ctrl+Alt+{fKeys[i - 1].ToString()}";
+            
+            int monitorIndex = i; // Capture for closure
+            contextMenu.Items.Add($"Toggle Monitor {i}  [{shortcut}]", null, 
+                (s, e) => ToggleMonitor(monitorIndex));
+        }
+
+        contextMenu.Items.Add("-"); // Separator
+
+        for (int i = 1; i <= monitorCount; i++)
+        {
+            int monitorIndex = i;
+            contextMenu.Items.Add($"Turn On Monitor {i}", null, 
+                (s, e) => TurnOnMonitor(monitorIndex));
+            contextMenu.Items.Add($"Turn Off Monitor {i}", null, 
+                (s, e) => TurnOffMonitor(monitorIndex));
+        }
+
+        contextMenu.Items.Add("-"); // Separator
         contextMenu.Items.Add("Exit", null, (s, e) => Application.Exit());
+
         notifyIcon.ContextMenuStrip = contextMenu;
         notifyIcon.Visible = true;
         this.WindowState = FormWindowState.Minimized;
@@ -52,74 +86,63 @@ public partial class Form1 : Form
         this.Hide();
     }
 
-    private void TurnOn1()
+    private void TurnOnMonitor(int monitor)
     {
-        RunCommand("monitorcontrol --set-power-mode on --monitor 1");
-        isMonitor1On = true;
+        RunCommand($"monitorcontrol --set-power-mode on --monitor {monitor}");
+        monitorStates[monitor - 1] = true;
     }
 
-    private void TurnOff1()
+    private void TurnOffMonitor(int monitor)
     {
-        RunCommand("monitorcontrol --set-power-mode off_soft --monitor 1");
-        isMonitor1On = false;
+        RunCommand($"monitorcontrol --set-power-mode off_soft --monitor {monitor}");
+        monitorStates[monitor - 1] = false;
     }
 
-    private void Toggle1()
+    private void ToggleMonitor(int monitor)
     {
-        if (isMonitor1On)
-            TurnOff1();
+        if (monitorStates[monitor - 1])
+            TurnOffMonitor(monitor);
         else
-            TurnOn1();
-    }
-
-    private void TurnOn2()
-    {
-        RunCommand("monitorcontrol --set-power-mode on --monitor 2");
-        isMonitor2On = true;
-    }
-
-    private void TurnOff2()
-    {
-        RunCommand("monitorcontrol --set-power-mode off_soft --monitor 2");
-        isMonitor2On = false;
-    }
-
-    private void Toggle2()
-    {
-        if (isMonitor2On)
-            TurnOff2();
-        else
-            TurnOn2();
+            TurnOnMonitor(monitor);
     }
 
     private void RunCommand(string args)
     {
-        Process.Start(new ProcessStartInfo
+        try
         {
-            FileName = "uvx",
-            Arguments = args,
-            UseShellExecute = false,
-            CreateNoWindow = true
-        });
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "uvx",
+                Arguments = args,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            });
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Error executing command: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
     }
 
     protected override void WndProc(ref Message m)
     {
         if (m.Msg == WM_HOTKEY)
         {
-            int id = m.WParam.ToInt32();
-            if (id == HOTKEY_ID1)
-                Toggle1();
-            else if (id == HOTKEY_ID2)
-                Toggle2();
+            int hotkeyId = m.WParam.ToInt32();
+            if (hotkeyId >= 1 && hotkeyId <= monitorCount)
+            {
+                ToggleMonitor(hotkeyId);
+            }
         }
         base.WndProc(ref m);
     }
 
     protected override void OnFormClosing(FormClosingEventArgs e)
     {
-        UnregisterHotKey(this.Handle, HOTKEY_ID1);
-        UnregisterHotKey(this.Handle, HOTKEY_ID2);
+        for (int i = 1; i <= monitorCount; i++)
+        {
+            UnregisterHotKey(this.Handle, i);
+        }
         notifyIcon.Dispose();
         base.OnFormClosing(e);
     }
